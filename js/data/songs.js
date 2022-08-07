d093a6a26bd6fa8e245ae2311e6a580bb1371bcf
CrwnChakra <devdevkev1@gmail.com>
2022-08-07 17:48:05 -0500

    music player pages

---
+++ js/data/songs.js
@@ -0,0 +1,85 @@
+export const songsList = [{
+    "id": "1",
+    "url": "../../music/satisfy-my-soul.mp3",
+    "time": "04:31",
+    "title": "Satisfy My Soul",
+    "artist": "Bob Marley"
+  },
+  {
+    "id": "2",
+    "url": "../../music/sweetest-taboo.mp3",
+    "time": "04:36",
+    "title": "Sweetest Taboo",
+    "artist": "Sade"
+  },
+  {
+    "id": "3",
+    "url": "../../music/remember-the-time.mp3",
+    "time": "03:59",
+    "title": "Remember The Time",
+    "artist": "Michael Jackson"
+  },
+  {
+    "id": "4",
+    "url": "../../music/my-prerogative.mp3",
+    "time": "04:45",
+    "title": "My Prerogative",
+    "artist": "Bobby Brown"
+  },
+  {
+    "id": "5",
+    "url": "../../music/circles.mp3",
+    "time": "03:18",
+    "title": "Circles",
+    "artist": "Durand Jones & The Indications"
+  },
+  {
+    "id": "6",
+    "url": "../../music/eternal-light.mp3",
+    "time": "3:13",
+    "title": "Eternal Light",
+    "artist": "Free Nationals & Chronixx"
+  },
+  {
+    "id": "7",
+    "url": "../../music/go-to-your-head.mp3",
+    "time": "04:00",
+    "title": "Don't Let It Get To Your Head",
+    "artist": "Brand Nubian"
+  },
+  {
+    "id": "8",
+    "url": "../../music/anything.mp3",
+    "time": "03:52",
+    "title": "Anything ft. Next",
+    "artist": "Jaheim"
+  },
+  {
+    "id": "9",
+    "url": "../../music/girl-only-knew.mp3",
+    "time": "03:54",
+    "title": "If Your Girl Only Knew",
+    "artist": "Aaliyah"
+  },
+  { 
+    "id": "10",
+    "url": "../../music/around-the-way-girl.mp3",
+    "time": "04:01",
+    "title": "Around The Way Girl",
+    "artist": "LL Cool J"
+  },
+  {
+    "id": "11",
+    "url": "../../music/the-mack.mp3",
+    "time": "03:46",
+    "title": "Return of The Mack",
+    "artist": "Mark Morrison"
+  },
+  {
+    "id": "12",
+    "url": "../../music/cant-tell-me-nothing.mp3",
+    "time": "04:31",
+    "title": "Tell Me Nothing",
+    "artist": "Kanye West"
+  }
+];
